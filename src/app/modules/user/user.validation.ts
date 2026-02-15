60cf60614366d5296be4bfcd7cf60b08f95a668b
Sabuj-Chowdhury <csabuj73@gmail.com>
2026-02-16 04:03:36 +0600

    ADD : create patient zod schema

---
+++ src/app/modules/user/user.validation.ts
@@ -0,0 +1,14 @@
+import z from "zod";
+
+export const createPatientZodValidation = z.object({
+  password: z.string(),
+  patient: {
+    name: z.string(),
+    email: z.email(),
+    contactNumber: z
+      .string()
+      .regex(/^(?:\+?880|0)1[3-9]\d{8}$/, "Invalid Bangladesh mobile number"),
+
+    address: z.string().optional(),
+  },
+});
