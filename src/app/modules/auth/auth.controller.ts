a5180944f6c9f57cea5d851d72319d95e8815f0f
Sabuj-Chowdhury <csabuj73@gmail.com>
2026-02-26 11:43:34 +0600

    ADD : when logged in set cookies

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -3,17 +3,28 @@ import tryAsync from "../../utils/tryAsync";
 import { AuthServices } from "./auth.service";
 import { sendResponse } from "../../utils/sendResponse";
 import httpStatus from "http-status";
+import { setCookies } from "../../utils/setCookies";
 
 const login = tryAsync(async (req: Request, res: Response) => {
   const payload = req.body;
 
   const result = await AuthServices.login(payload);
 
+  const { accessToken, refreshToken, needPasswordChange } = result;
+
+  const tokenInfo = {
+    accessToken,
+    refreshToken,
+  };
+
+  // setting the cookie
+  setCookies(res, tokenInfo);
+
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
     message: "User Logged in successfully!",
-    data: result,
+    data: { needPasswordChange: needPasswordChange },
   });
 });
 
