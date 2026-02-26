e3c3f6f7ea0eaa178724cae51e239034e1f328cf
Sabuj-Chowdhury <csabuj73@gmail.com>
2026-02-26 12:53:53 +0600

    ADD : zod validation and route for create admin

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -2,13 +2,25 @@ import { Router } from "express";
 import { UserController } from "./user.controller";
 import { multerUpload } from "../../config/multer.config";
 import { validateRequest } from "../../middlewares/validateRequest";
-import { createPatientZodValidation } from "./user.validation";
+import {
+  createAdminZodValidation,
+  createPatientZodValidation,
+} from "./user.validation";
 
 export const userRouter = Router();
 
+// create user route
 userRouter.post(
   "/create-user",
   multerUpload.single("file"),
   validateRequest(createPatientZodValidation),
   UserController.createUser,
+);
+
+// create admin  --TODO : check auth
+userRouter.post(
+  "/create-admin",
+  multerUpload.single("file"),
+  validateRequest(createAdminZodValidation),
+  UserController.cerateAdmin,
 );
--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -12,3 +12,14 @@ export const createPatientZodValidation = z.object({
     address: z.string().optional(),
   }),
 });
+
+export const createAdminZodValidation = z.object({
+  password: z.string(),
+  admin: z.object({
+    name: z.string(),
+    email: z.email(),
+    contactNumber: z
+      .string()
+      .regex(/^(?:\+?880|0)1[3-9]\d{8}$/, "Invalid Bangladesh mobile number"),
+  }),
+});
