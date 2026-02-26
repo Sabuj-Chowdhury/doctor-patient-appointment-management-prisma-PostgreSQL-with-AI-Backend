d9b426debcaf1bc2120535ae6388b9645f58f752
Sabuj-Chowdhury <csabuj73@gmail.com>
2026-02-26 14:26:52 +0600

    ZOD : doctor , user, admin zod validation

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -1,9 +1,10 @@
+import { Gender } from "@prisma/client";
 import z from "zod";
 
 export const createPatientZodValidation = z.object({
-  password: z.string(),
+  password: z.string({ error: "password is required!" }),
   patient: z.object({
-    name: z.string(),
+    name: z.string({ error: "name is required!" }),
     email: z.email(),
     contactNumber: z
       .string()
@@ -14,12 +15,31 @@ export const createPatientZodValidation = z.object({
 });
 
 export const createAdminZodValidation = z.object({
-  password: z.string(),
+  password: z.string({ error: "password is required!" }),
   admin: z.object({
-    name: z.string(),
+    name: z.string({ error: "name is required!" }),
     email: z.email(),
     contactNumber: z
-      .string()
+      .string({ error: "contact number is required!" })
       .regex(/^(?:\+?880|0)1[3-9]\d{8}$/, "Invalid Bangladesh mobile number"),
+  }),
+});
+
+export const createDoctorZodValidation = z.object({
+  password: z.string({ error: "password is required!" }),
+  doctor: z.object({
+    name: z.string({ error: "name is required!" }),
+    email: z.email(),
+    contactNumber: z
+      .string({ error: "contact number is required!" })
+      .regex(/^(?:\+?880|0)1[3-9]\d{8}$/, "Invalid Bangladesh mobile number"),
+    address: z.string().optional(),
+    registrationNumber: z.string({ error: "registration number is required!" }),
+    experience: z.number().optional(),
+    gender: z.enum([Gender.MALE, Gender.FEMALE]),
+    appointmentFee: z.number({ error: "appointment fee is required!" }),
+    qualification: z.string({ error: "qualification is required!" }),
+    currentWorkingPlace: z.string({ error: "current working place required!" }),
+    designation: z.string({ error: " designation is required!" }),
   }),
 });
