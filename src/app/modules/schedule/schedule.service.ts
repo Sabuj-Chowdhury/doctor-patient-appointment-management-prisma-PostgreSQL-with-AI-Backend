390ed04299f1fd647cd2d972eb62599f241f5c1d
Sabuj-Chowdhury <csabuj73@gmail.com>
2026-03-19 09:54:14 +0600

    schedules create with interval and date for the slot

--- src/app/modules/schedule/schedule.service.ts
+++ src/app/modules/schedule/schedule.service.ts
@@ -1,9 +1,14 @@
 import { addHours, addMinutes, format } from "date-fns";
+import { prisma } from "../../config/prismaInstance";
 
 const createSchedules = async (payload: any) => {
   //   console.log({ payload });
 
+  const createdSchedule = [];
+
   const { startDate, endDate, startTime, endTime } = payload;
+
+  const interval = 30;
 
   const startingDate = new Date(startDate);
   const endingDate = new Date(endDate);
@@ -13,7 +18,7 @@ const createSchedules = async (payload: any) => {
     const startingDateTime = new Date(
       addMinutes(
         addHours(
-          `${format(startingDate, "yyyy-mm-dd")}`,
+          `${format(startingDate, "yyyy-MM-dd")}`,
           Number(startTime.split(":")[0]), //10:00
         ),
 
@@ -24,7 +29,7 @@ const createSchedules = async (payload: any) => {
     const endingDateTime = new Date(
       addMinutes(
         addHours(
-          `${format(startingDate, "yyyy-mm-dd")}`,
+          `${format(startingDate, "yyyy-MM-dd")}`,
           Number(endTime.split(":")[0]), //10:00
         ),
 
@@ -32,10 +37,39 @@ const createSchedules = async (payload: any) => {
       ),
     );
 
-    console.log({ startingDateTime, endingDateTime });
+    // console.log({ startingDateTime, endingDateTime });
+    while (startingDateTime < endingDateTime) {
+      const slotStartDateTime = startingDateTime;
+      const slotEndDateTime = addMinutes(startingDateTime, interval);
+
+      const scheduleData = {
+        startDateTime: slotStartDateTime,
+        endDateTime: slotEndDateTime,
+      };
+
+      const existingSchedule = await prisma.schedule.findFirst({
+        where: scheduleData,
+      });
+
+      // if no schedule was created before then create the schedule
+      if (!existingSchedule) {
+        const schedule = await prisma.schedule.create({
+          data: scheduleData,
+        });
+
+        // push the schedule in the schedules created
+        createdSchedule.push(schedule);
+      }
+
+      // change the slot startDateTime
+      slotStartDateTime.setMinutes(slotStartDateTime.getMinutes() + interval);
+    }
+
+    // change the date
+    startingDate.setDate(startingDate.getDate() + 1);
   }
 
-  return "payload";
+  return createdSchedule;
 };
 
 export const schedulesService = {
