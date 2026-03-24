3fe467ee5b0564b0196314432d694fff5ca723f1
Sabuj-Chowdhury <csabuj73@gmail.com>
2026-03-24 15:02:55 +0600

    primsa client known error handling

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,3 +1,4 @@
+import { Prisma } from "@prisma/client";
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status";
 
@@ -7,10 +8,29 @@ const globalErrorhandler = (
   res: Response,
   next: NextFunction,
 ) => {
-  let statusCode = httpStatus.INTERNAL_SERVER_ERROR;
-  let success = false;
+  const statusCode = httpStatus.INTERNAL_SERVER_ERROR;
+  const success = false;
   let message = err.message || "Something went wrong!";
   let error = err;
+
+  if (err instanceof Prisma.PrismaClientKnownRequestError) {
+    if (err.code === "P2002") {
+      message = "Duplicate key error!";
+      error = err.meta;
+    }
+    if (err.code === "P1000") {
+      message = "Authentication failed against database server!";
+      error = err.meta;
+    }
+    if (err.code === "P1017") {
+      message = "Server has closed the connection!";
+      error = err.meta;
+    }
+    if (err.code === "P2006") {
+      message = "The provided value for field is not valid!";
+      error = err.meta;
+    }
+  }
 
   res.status(statusCode).send({
     success,
